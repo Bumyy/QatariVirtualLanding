83446877cf8d0a823e61d7ef075c01f592bd7218
Bumyy <57840717+Bumyy@users.noreply.github.com>
2026-08-13 18:00:48 +0200

    fix link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  site: 'https://qatarivirtual.xyz',
+  output: "static",
+  site: "https://qatarivirtual.com",
   integrations: [react(), sitemap()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
